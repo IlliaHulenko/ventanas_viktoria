70d8afcbf59a7aa0fffc5432eccc70b75f348189
Illia Hulenko <73845251+IlliaHulenko@users.noreply.github.com>
2024-11-27 03:02:08 +0100

    To a Hero section added an animation

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,11 @@
-import { useRef, useState } from 'react'
+import { useEffect, useRef, useState } from 'react'
 import Button from './Button';
+import { TiLocationArrow } from 'react-icons/ti';
+import { useGSAP } from '@gsap/react';
+import gsap from 'gsap';
+import { ScrollTrigger } from "gsap/all";
+
+gsap.registerPlugin(ScrollTrigger);
 
 const Hero = () => {
 
@@ -23,11 +29,70 @@ const Hero = () => {
         setCurrentIndex(upcomingVideoIndex);
     }
 
+    useEffect(() => {
+        if (loadedVideos === totalVideos - 1) {
+            setIsLoading(false);
+        }
+    }, [loadedVideos]);
+
+    useGSAP(() => {
+        if(hasClicked){
+            gsap.set('#next-video', {visibility: 'visible'});
+                
+            gsap.to('#next-video', {
+                transformOrigin: 'center center',
+                scale: 1,
+                width: '100%',
+                height: '100%',
+                duration: 1,
+                ease: 'power1.inOut',
+                onStart: () => nextVideoRef.current.play()
+            })
+
+            gsap.from('#current-video', {
+                transformOrigin: 'center center',
+                scale: 0,
+                duration: 1.5,
+                ease: 'power1.inOut'
+            })
+        }
+    }, {dependencies: [currentIndex], revertOnUpdate: true});
+
+    useGSAP(() => {
+        gsap.set('#video-frame', {
+            clipPath: 'polygon(14% 0, 72% 0, 88% 90%, 0 95%)',
+            borderRadius: '0% 0% 40% 10%'
+        });
+
+        gsap.from("#video-frame", {
+            clipPath: "polygon(0% 0%, 100% 0%, 100% 100%, 0% 100%)",
+            borderRadius: "0% 0% 0% 0%",
+            ease: "power1.inOut",
+            scrollTrigger: {
+              trigger: "#video-frame",
+              start: "center center",
+              end: "bottom center",
+              scrub: true,
+            },
+          });
+    })
+
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`
 
 
   return (
     <div className='relative h-dvh w-screen overflow-x-hidden'>
+
+        {isLoading && (
+            <div className="flex-center absolute z-[100] h-dvh w-screen overflow-hidden bg-violet-50">
+                <div className='three-body'>
+                    <div className="three-body__dot"></div>
+                    <div className="three-body__dot"></div>
+                    <div className="three-body__dot"></div>
+                </div>
+            </div>
+        )}
+
         <div 
             id='video-frame' 
             className='relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75'
@@ -65,11 +130,11 @@ const Hero = () => {
                 />
 
                 <video 
-                    src={getVideoSrc(currentIndex === totalVideos -1 ? 1 : currentIndex)}
+                    src={getVideoSrc(currentIndex === totalVideos - 1 ? 1 : currentIndex)}
                     autoPlay
                     loop
                     muted
-                    className='absolute left-0 top-0 size-full object-cover object-center'
+                    className="absolute left-0 top-0 size-full object-cover object-center"
                     onLoadedData={handleVideoLoad}
                 />
             </div>
@@ -85,10 +150,19 @@ const Hero = () => {
                     Los componentes de alta calidad certificados en la Unión Europea <br/>
                     garantizan la calidad, durabilidad y alto rendimiento de nuestras estructuras
                     </p>
-                    <Button />
+                    {/* TODO: Verify name of an ID and change Title */}
+                    <Button 
+                        id='watch-trailer' 
+                        title='Ver más' 
+                        leftIcon={<TiLocationArrow />} 
+                        containerClass="bg-yellow-300 flex-center gap-1"
+                    />
                 </div>
             </div>
         </div>
+        <h1 className='special-font hero-heading absolute bottom-5 right-5 text-black'>
+            Ventanas Viktoria
+        </h1>
     </div>
   )
 }
