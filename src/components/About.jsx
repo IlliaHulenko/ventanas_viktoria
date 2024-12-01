cd42ca0ac5056c13f37e3c382f562e2aad9e27b2
Illia Hulenko <73845251+IlliaHulenko@users.noreply.github.com>
2024-12-01 23:36:50 +0100

    Added About page

---
+++ src/components/About.jsx
@@ -0,0 +1,64 @@
+import { useGSAP } from '@gsap/react'
+import gsap from 'gsap'
+import React from 'react'
+
+import { ScrollTrigger } from 'gsap/all'
+
+gsap.registerPlugin(ScrollTrigger);
+
+const About = () => {
+
+    useGSAP(() => {
+        const clipAnimation = gsap.timeline({
+          scrollTrigger: {
+            trigger: "#clip",
+            start: "center center",
+            end: "+=800 center",
+            scrub: 0.5,
+            pin: true,
+            pinSpacing: true,
+          },
+        });
+    
+        clipAnimation.to(".mask-clip-path", {
+          width: "100vw",
+          height: "100vh",
+          borderRadius: 0,
+        });
+    });
+
+
+  return (
+    <div id='about' className='min-h-screen w-screen'>
+        <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+            <h2 className='font-general text-sm uppercase md:text-[10px]'>
+                Ventanas de alta calidad
+            </h2>
+            
+            <div className='mt-5 text-center text-4xl uppercase leading-[0.8] 
+                md:text-[6rem]'
+            >
+                Descubre la mejor relación <br/> calidad-precio en el mercado
+            </div>
+            
+            <div className='about-subtext'>
+                <p>Producción rápida</p>
+                <p>
+                    La fabricación de estructuras es una ventaja significativa para nuestros clientes
+                </p>
+            </div>            
+        </div>
+        <div className='h-dvh w-screen' id='clip'>
+            <div className='mask-clip-path about-image'>
+                <img 
+                    src='img/about.jpg'
+                    alt='Background image'
+                    className='absolute left-0 top-0 size-full object-cover'
+                />
+            </div>
+        </div>
+    </div>
+  )
+}
+
+export default About
