65244b263f948ef840bf851480f5556373678caa
Illia Hulenko <73845251+IlliaHulenko@users.noreply.github.com>
2024-12-09 23:19:32 +0100

    Added changes for deploying to GitHub pages

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -10,20 +10,29 @@ const Navbar = () => {
 
   const [lastScrollY, setLastScrollY] = useState(0);
   const [isNavVisible, setIsNavVisible] = useState(true);
+  const [isContactButtonVisible, setIsContactButtonVisible] = useState(true);
   const { y: currentScrollY } = useWindowScroll();
 
   const navContainerRef = useRef(null);  
+
+  const contactButtonRef = useRef(null);
 
   useEffect(() => {
     if(currentScrollY === 0 ){
       setIsNavVisible(true);
       navContainerRef.current.classList.remove('floating-nav');
+      setIsContactButtonVisible(true);
+      contactButtonRef.current.classList.remove('contact-us-btn');
     } else if(currentScrollY > lastScrollY){
       setIsNavVisible(false);
       navContainerRef.current.classList.add('floating-nav');
+      setIsContactButtonVisible(false);
+      contactButtonRef.current.classList.add('contact-us-btn');
     } else if(currentScrollY < lastScrollY){
       setIsNavVisible(true);
       navContainerRef.current.classList.add('floating-nav');
+      setIsContactButtonVisible(true);
+      contactButtonRef.current.classList.add('contact-us-btn');
     }
 
     setLastScrollY(currentScrollY);
@@ -35,43 +44,58 @@ const Navbar = () => {
       opacity: isNavVisible ? 1 : 0,
       duration: 0.5
     })
-  }, [isNavVisible]);
+    gsap.to(contactButtonRef.current, {
+      x: isContactButtonVisible ? 0 : -100,
+      opacity: isContactButtonVisible ? 1 : 0,
+      duration: 0.7
+    })
+  }, [isNavVisible, isContactButtonVisible]);
 
   return (
-    <div
-      ref={navContainerRef}
-      className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all 
-        duration-700 sm:inset-x-6'    
-    >
-      <header className='absolute top-1/2 w-full -translate-y-1/2'>
-        <nav className='flex size-full items-center justify-between p-4'>
-          {/* Navbar's left side */}
-          <div className='flex items-center gap-7'>
-            <img src='/img/logo.svg' alt='logo' className='w-10'/>
-            <Button 
-              id="product-button"
-              title="Productos"
-              rightIcon={<GiWindow />} 
-              containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
-            />
-          </div>
-          {/* Navbar's right side */}
-          <div className='flex h-full items-center'>            
-            <div className='hidden md:block'>
-              {navItems.map((item) => (
-                <a 
-                  key={item} 
-                  href={`#${item.toLowerCase()}`}
-                  className='nav-hover-btn'
-                >
-                  {item}
-                </a>
-              ))}
+    <>    
+      <div
+        ref={navContainerRef}
+        className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all 
+          duration-700 sm:inset-x-6'    
+      >
+        <header className='absolute top-1/2 w-full -translate-y-1/2'>
+          <nav className='flex size-full items-center justify-between p-4'>
+            {/* Navbar's left side */}
+            <div className='flex items-center gap-7'>
+              <img src='/img/logo.svg' alt='logo' className='w-10'/>
+              <Button 
+                id="product-button"
+                title="Productos"
+                rightIcon={<GiWindow />} 
+                containerClass="bg-blue-50 md:flex hidden items-center justify-center gap-1"
+              />
             </div>
-          </div>
-        </nav>
-      </header>      
-    </div>
+            {/* Navbar's right side */}
+            <div className='flex h-full items-center'>            
+              <div className='hidden md:block'>
+                {navItems.map((item) => (
+                  <a 
+                    key={item} 
+                    href={`#${item.toLowerCase()}`}
+                    className='nav-hover-btn'
+                  >
+                    {item}
+                  </a>
+                ))}
+              </div>
+            </div>
+          </nav>
+        </header>
+      </div>
+      {/* Contact Us button */}
+      <div     
+        ref={contactButtonRef}    
+        className='contact-us-btn'
+      >
+          <span className='absolute'>Contact Us</span>
+      </div>
+      
+    </>
   )
 }
 
