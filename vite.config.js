debdd8c3b132d30f0f6f9d6007628545923e94f6
Illia Hulenko <73845251+IlliaHulenko@users.noreply.github.com>
2025-02-13 10:08:53 +0100

    Deploy GHPages commit base:'/' with a custom  deploy.yml

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],  
-  base: "/ventanas_viktoria/" 
+  plugins: [react()], 
+  base: '/'   
 })
